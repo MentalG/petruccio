33c0874b4ba8cd6778d74394e9795f21448ceb9a
Illia Hromov <i.hromov@stollerukraine.com>
2019-12-16 17:18:34 +0200

    navbar added

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './app.css';
+import Navbar from '../navbar/navbar';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	return (
+		<div className='App'>
+      <Navbar />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+//styles
+import './navbar.css'
+
+//materials
+import logo from '../../materials/images/logo.png'
+import cart from '../../materials/images/cart.png'
+import menu from '../../materials/images/menu.svg'
+
+const Navbar = () => {
+    return(
+        <div className="navbar">
+            <div className='navbar-menu'>
+                <img src={menu} alt="menu"/>
+                <div>Menu</div>
+            </div>
+            <div className='navbar-image' style={{ backgroundImage: `url(${logo})` }}/>
+            <div className='navbar-cart' >
+                <img src={cart} alt={'cart'}/>
+                <div>{`Cart(${0})`}</div>
+                <div>Login</div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
